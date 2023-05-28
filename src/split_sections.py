6d863dccd120e6572f8ba524bc8ab46d68aaf2b4
Amit Moryossef <amitmoryossef@gmail.com>
2023-05-28 14:43:11 +0200

    feat(text-to-gloss): add paper and reservations

--- src/split_sections.py
+++ src/split_sections.py
@@ -1,42 +1,66 @@
 import os
 import regex as re
 
-with open("dst/index.tex", "r") as f:
-    text = f.read()
 
-# Correct assets path
-text = text.replace("assets/", "parts/background/assets/")
-# Remove lineheight
-text = text.replace(",height=\\textheight", "")
+def correct_latex_text(text):
+    text = text.replace("assets/", "parts/background/assets/")
+    text = text.replace(",height=\\textheight", "")  # remoe lineheight from figures
+    return text
 
-matches = re.findall(r'(\\([sub]*)section\{([\s\S]*?)}\\label{([\s\S]*?)}\}([\s\S]*?))\\(sub)*section', text,
-                     overlapped=True)
-print("matches", len(matches))
 
-dir_name = "dst/sections"
-os.makedirs(dir_name, exist_ok=True)
+def find_sections(text):
+    return re.findall(r'(\\([sub]*)section\{([\s\S]*?)}(\\label{([\s\S]*?)}\}?)?([\s\S]*?))\\(sub)*section', text,
+                      overlapped=True)
 
-labels = []
-last_level = ""
 
-for full_match, level, section, label, content, _ in matches:
+def write_section_to_file(output_dir, level, section, label, content, labels, section_prefix="sec:"):
     section = section.replace("\n", " ")
+    section_name = label.replace("sec:", "")
 
     level_num = len(level) // len('sub')
     if level_num >= len(labels):
-        labels.append(label)
+        labels.append(section_name)
     else:
-        labels[level_num] = label
+        labels[level_num] = section_name
     labels = labels[:level_num + 1]  # cut off the rest
 
-    new_label = "-".join(labels)
-    f_dir = os.path.join(dir_name, "/".join(labels[:-1]))
+    new_label = section_prefix + "-".join(labels).replace("sec:", "")
+    f_dir = os.path.join(output_dir, "/".join(labels[:-1]))
     os.makedirs(f_dir, exist_ok=True)
-    f_name = os.path.join(f_dir, label + ".tex")
+    f_name = os.path.join(f_dir, section_name + ".tex")
     with open(f_name, "w") as f:
-        if len(level) > 0:
-            f.write(f"% \\{level}section{{{section}}}\\label{{{new_label}}}\n")
+        # if len(level) > 0:
+        f.write(f"% \\{level}section{{{section}}}\\label{{{new_label}}}\n")
         content_rows = content.splitlines()[:-1]
         f.write("\n".join(content_rows))
 
-    print(f_name)
+    return labels
+
+
+def split_sections(file_path, output_dir="dst/sections", section_prefix=""):
+    os.makedirs(output_dir, exist_ok=True)
+
+    with open(file_path, "r") as f:
+        text = f.read()
+
+    text = correct_latex_text(text)
+    matches = find_sections(text)
+    # print("matches", len(matches))
+
+    abstract = re.search(r'\\begin\{abstract\}([\s\S]*?)\\end\{abstract\}', text)
+    if abstract:
+        with open(os.path.join(output_dir, "abstract.tex"), "w") as f:
+            f.write(abstract.group(1))
+
+    labels = []
+    for full_match, level, section, _, label, content, _ in matches:
+        if label == "":
+            label = section.replace(" ", "-").lower()
+        labels = write_section_to_file(output_dir, level, section, label, content, labels, section_prefix)
+        # print(f"Written section to {label}.tex")
+
+
+if __name__ == "__main__":
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    dir_name = os.path.join(current_dir, "..", "dst", "sections")
+    split_sections(os.path.join(current_dir, "..", "dst", "index.tex"), dir_name, section_prefix="")
